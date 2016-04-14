4d4aa2fa7457e5ca6a8251d2eca1c20e97f0e4e7
Daniel Vianna <dvianna@bunnings.com.au>
2016-04-14 13:09:18 +0800

    init.el with better Haskell and PureScript support

--- init.el
+++ init.el
@@ -18,14 +18,14 @@
 (windmove-default-keybindings 'shift)
 (global-set-key (kbd "C-e") 'eshell)
 
-;; Save all tempfiles in $TMPDIR/emacs$UID/                                                        
-    (defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
-    (setq backup-directory-alist
-        `((".*" . ,emacs-tmp-dir)))
-    (setq auto-save-file-name-transforms
-        `((".*" ,emacs-tmp-dir t)))
-    (setq auto-save-list-file-prefix
-        emacs-tmp-dir)
+;; Save all tempfiles in $TMPDIR/emacs$UID/
+(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
 
 ;; Set tab width to 4
 (setq-default indent-tabs-mode nil)
@@ -48,16 +48,9 @@
 (package-initialize)
 (add-to-list 'package-archives
              ;; '("marmalade" . "http://marmalade-repo.org/packages/")
-             '("melpa-stable" . "http://stable.melpa.org/packages/")
-             ;; '("melpa" . "http://melpa.org/packages/")
+             ;;'("melpa-stable" . "http://stable.melpa.org/packages/")
+             '("melpa" . "http://melpa.org/packages/")
              t)
-
-(defadvice package-compute-transaction
-  (before package-compute-transaction-reverse (package-list requirements) activate compile)
-    "reverse the requirements"
-    (setq requirements (reverse requirements))
-    (print requirements))
-
 
 ;; Solarized theme
 
@@ -66,7 +59,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
+ '(custom-safe-themes
+   (quote
+    ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ ;; '(haskell-process-auto-import-loaded-modules t)
+ ;; '(haskell-process-log t)
+ ;; '(haskell-process-suggest-remove-import-lines t)
+ ;; '(haskell-process-type (quote cabal-repl))
+ ;; '(haskell-tags-on-save t)
+ ;; '(purescript-mode-hook
+ ;;   (quote
+ ;;    (turn-on-purescript-indent))
+ ;;    (quote
+ ;;     (psc-ide-mode company-mode))
+ ;;    )
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -93,6 +100,24 @@
 ;;;; Syntax checking (Flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+;;;; Autocompletion
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;;;; Tagging
+(require 'etags-table)
+(setq etags-table-search-up-depth 10)
+
+;;;; Haskell Autocompletion
+;;(add-hook 'haskell-mode-hook 'company-mode)
+(require 'ghc)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+(add-to-list 'company-backends 'company-ghc)
+(custom-set-variables '(company-ghc-show-info t))
+
+;;;; Haskell-mode indenting and navigation
+(add-hook 'haskell-mode-hook #'hindent-mode)
+(custom-set-variables '(haskell-tags-on-save t))
 
 ;;; Haskell
 
@@ -104,12 +129,7 @@
 (let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
   (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
   (add-to-list 'exec-path my-cabal-path))
-(custom-set-variables '(haskell-tags-on-save t))
 
-(custom-set-variables
-  '(haskell-process-suggest-remove-import-lines t)
-  '(haskell-process-auto-import-loaded-modules t)
-  '(haskell-process-log t))
 (eval-after-load 'haskell-mode '(progn
   (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
   (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
@@ -117,15 +137,38 @@
   (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
   (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
   (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
-  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+  (define-key haskell-mode-map [f8] 'haskell-navigate-imports)
+  ))
 (eval-after-load 'haskell-cabal '(progn
   (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
   (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
   (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
   (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
-(custom-set-variables '(haskell-process-type 'cabal-repl))
+(custom-set-variables '(haskell-process-type 'stack-ghci))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PureScript
+
+(require 'purescript-mode)
+(add-hook 'purescript-mode-hook 'turn-on-purescript-indent)
+
+;; Flycheck PureScript
+(eval-after-load 'flycheck
+  '(flycheck-purescript-setup))
+
+;; PureScript IDE
+(customize-set-variable 'psc-ide-executable "~/.local/bin/psc-ide-server")
+(require 'psc-ide)
+(add-hook 'psc-ide-mode 'company-mode)
+
+;; PuresScript repl (emacs-psci)
+(add-hook 'purescript-mode-hook 'inferior-psci-mode)
+(require 'repl-toggle)
+(require 'psci)
+(add-to-list 'rtog/mode-repl-alist '(purescript-mode . psci))
+
+;;;;;;;;;;;;;;;;;;;;
 ;;; Python
 
 ;; Default virtualenv to "py" -- may be useful when reinstalling jedi
@@ -162,33 +205,33 @@
 
 ;; IPython
 
-;; IPython shell:
+;; ;; IPython shell:
 
-(if new_python 
-    (setq
-     python-shell-interpreter "ipython"
-     python-shell-interpreter-args ""
-     python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-     python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-     python-shell-completion-setup-code
-     "from IPython.core.completerlib import module_completion"
-     python-shell-completion-string-code
-     "';'.join(module_completion('''%s'''))\n"
-     python-shell-completion-string-code
-     "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-  )
+;; (if new_python 
+;;     (setq
+;;      python-shell-interpreter "ipython"
+;;      python-shell-interpreter-args ""
+;;      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+;;      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+;;      python-shell-completion-setup-code
+;;      "from IPython.core.completerlib import module_completion"
+;;      python-shell-completion-string-code
+;;      "';'.join(module_completion('''%s'''))\n"
+;;      python-shell-completion-string-code
+;;      "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+;;   )
 
-(if nil ;new_python
-    (progn
-      (require 'ein)
-      (setq-default py-python-command-args '("--colors" "DarkBG"))
-      (setq ein:use-auto-complete t)
-      )
-  nil)
+;; (if nil ;new_python
+;;     (progn
+;;       (require 'ein)
+;;       (setq-default py-python-command-args '("--colors" "DarkBG"))
+;;       (setq ein:use-auto-complete t)
+;;       )
+;;   nil)
 
 ;; Enable Git
-(require 'magit)
-(global-set-key (kbd "C-g") 'magit-status)
+;; (require 'magit)
+;; (global-set-key (kbd "C-g") 'magit-status)
 
 ;; Enable web-mode
 (require 'web-mode)
@@ -210,6 +253,63 @@
   '(progn
   (require 'tern-auto-complete)
   (tern-ac-setup)))
+
+;; Enable ES6
+;; disable jshint since we prefer eslint
+(require 'flycheck)
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; JSX syntax highlighting
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
+
+;; (flycheck-define-checker jsxhint-checker
+;;   "A JSX syntax and style checker based on JSXHint."
+
+;;   :command ("jsxhint" source)
+;;   :error-patterns
+;;   ((error line-start (1+ nonl) ": line " line ", col " column ", "
+;;     (message) line-end))
+;;   :modes (web-mode))
+;; (add-hook 'web-mode-hook
+;;           (lambda ()
+;;             (when (equal web-mode-content-type "jsx")
+;;               ;; enable flycheck
+;;               (flycheck-select-checker 'jsxhint-checker)
+;;               (flycheck-mode))))
+
+;; (add-hook 'jsx-mode-hook
+;;           (lambda () (auto-complete-mode 1)))
+
+;; Stylus and jade modes
+;;(add-to-list 'load-path "~/.emacs.d/vendor/jade-mode")
+    (require 'sws-mode)
+    (require 'jade-mode)
+    (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+
+;; xterm-color.el
+
+(progn (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+       (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
+        (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region))
+ 
+(progn (remove-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+       (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
+       (setq font-lock-unfontify-region-function 'font-lock-default-unfontify-region))
+
+
+
 
 (provide 'init)
 ;;; init.el ends here
