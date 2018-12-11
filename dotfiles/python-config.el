cd537cf48bf0fa37641c9c710f94190d5d4c2851
Daniel Vianna <dvianna@ausnetservices.com.au>
2018-12-11 11:05:26 +1100

    Cleanup; Add both flycheck-mypy and elpy with flake8/pylint

--- dotfiles/python-config.el
+++ dotfiles/python-config.el
@@ -4,14 +4,20 @@
 ;;; Python development environment
 ;;;
 ;;; Commentary:
+;;; `flycheck-mypy` provides mypy support (static type validation in Python).
+;;; This does not work with flymake (used by elpy).
+;;; `elpy` provides a lot of IDE support, including running both `flake8` and
+;;; `pylint` via flymake. I wasn't able to run either alongside mypy on
+;;; flycheck, but they seem to work well in parallel flycheck and flymake
+;;; processes.
+;;;
+;;; Everything hinges on selecting a `pyvenv-workon` *prior* to opening any
+;;; Python file. After that, one has to restart emacs and try again.
+;;;
 ;;; Code:
 
 (require 'flycheck-mypy)
-(add-hook 'python-mode-hook 'flycheck-mode)
-(add-to-list 'flycheck-disabled-checkers 'python-flake8)
-(add-to-list 'flycheck-disabled-checkers 'python-pylint)
 
-(remove-hook 'elpy-modules 'elpy-module-flymake)
 (elpy-enable)
 
 (provide 'python-config)
