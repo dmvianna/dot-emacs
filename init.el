71cf014e7768b7173c47f93922f28ef2ea3cc141
Daniel Vianna <dmlvianna@gmail.com>
2019-04-26 21:08:17 +1000

    add racer (rust autocompletion via company-mode)

--- init.el
+++ init.el
@@ -150,6 +150,12 @@
 ;; Rust
 ;; (require 'rust-config)
 (setq rust-format-on-save t)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
 
 ;; rainbow-mode for CSS
 (require 'rainbow-mode)
