8f4105c2186b72f3356b430df63df7e01abe5782
Daniel Vianna <daniel.vianna@ausnetservices.com.au>
2020-10-07 15:16:14 +1100

    add jupyter as python interpreter for elpy

--- dotfiles/python-config.el
+++ dotfiles/python-config.el
@@ -29,4 +29,10 @@
  :after python)
 (elpy-enable)
 
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
+
 (provide 'python-config)
