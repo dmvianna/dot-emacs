d28a4a9e51da83182f6c0397e1ce5f3dde576fb8
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-06-01 09:59:51 +1000

    add haskell keybindings

--- dotfiles/haskell-config.el
+++ dotfiles/haskell-config.el
@@ -12,6 +12,7 @@
   :commands 'dante-mode
   :init
   (add-hook 'haskell-mode-hook 'dante-mode)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
   (add-hook 'haskell-mode-hook 'flycheck-mode)
   )
 
@@ -31,6 +32,14 @@
  '(haskell-stylish-on-save 't)
  '(dante-repl-command-line '("cabal" "new-repl" dante-target))
  )
+
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
 
 (provide 'haskell-config)
 
